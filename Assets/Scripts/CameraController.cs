b29442c482780761436b0fec88a4612202b67dd6
B-U-T-T-E-R <stas.nikolaev.2019@gmail.com>
2022-08-07 20:11:04 +0300

    new branch, new mobs, new skills and new tiles

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,15 +7,19 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) && transform.position.z > -180)
+        if(Input.GetKey(KeyCode.W) && transform.position.y > -65)
             transform.Translate(0, -speedCamera * Time.deltaTime, 0);
-        if(Input.GetKey(KeyCode.A) && transform.position.x < 625)
+        if(Input.GetKey(KeyCode.A) && transform.position.x < 430)
             transform.Translate(speedCamera * Time.deltaTime, 0, 0);
-        if(Input.GetKey(KeyCode.S) && transform.position.z < 180)
+        if(Input.GetKey(KeyCode.S) && transform.position.y < 65)
             transform.Translate(0, speedCamera * Time.deltaTime, 0);
-        if(Input.GetKey(KeyCode.D) && transform.position.x > -625)
+        if(Input.GetKey(KeyCode.D) && transform.position.x > -430)
             transform.Translate(-speedCamera * Time.deltaTime, 0, 0);
+        if(Input.GetKey(KeyCode.O) && transform.position.z > -1500)
+            transform.Translate(0, 0, -speedCamera * Time.deltaTime);
+        if(Input.GetKey(KeyCode.I) && transform.position.z < -500)
+            transform.Translate(0, 0, speedCamera * Time.deltaTime);
         if(Input.GetKeyDown(KeyCode.Tab))
-            transform.position = new Vector3(0, 1000, 0);
+            transform.position = new Vector3(0, 0, -1000);
     }
 }
