6aa437a4be89dcfb25eb8e68ab22cbfab47caaa1
B-U-T-T-E-R <stas.nikolaev.2019@gmail.com>
2022-08-19 20:10:19 +0300

    I do not know what to write

--- Assets/Scripts/BaseContoller.cs
+++ Assets/Scripts/BaseContoller.cs
@@ -1,12 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BaseContoller : MonoBehaviour
 {
-    void Start()
-    {
-    }
-
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private Text hpBar;
     void Update()
     {
+        hpBar.text = health.ToString();
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+            Die();
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("On Enter");
+        /*if(other.tag == "Common")
+            health -= 1;
+        if(other.tag == "Speedy")
+            health -= 2;
+        if(other.tag == "Healthy")
+            health -= 5;*/
+    }
+
+    void Die()
+    {
+        if(health == 0)
+            SceneManager.LoadScene(0);
     }
 }
