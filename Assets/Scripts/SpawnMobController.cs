d6cb9caa7d8c303e81fca7ebff9659e2f239fe0d
B-U-T-T-E-R <stas.nikolaev.2019@gmail.com>
2022-12-16 20:52:40 +0300

    changed code v9.1

--- Assets/Scripts/SpawnMobController.cs
+++ Assets/Scripts/SpawnMobController.cs
@@ -7,17 +7,21 @@ public class SpawnMobController : MonoBehaviour
     [SerializeField]
     private GameObject[] mobs;
     [SerializeField]
+    private bool canSpawn;
+    [SerializeField]
     private int maxCountMobs;
     private GameObject mob;
     [HideInInspector]
     public int currentCountMobsOnWave;
     public Transform[] wayPoints;
 
+    void Start() => canSpawn = true;
+
     void Update() => Spawn();
 
     private void Spawn()
     {
-        if(currentCountMobsOnWave != maxCountMobs)
+        if(currentCountMobsOnWave != maxCountMobs && canSpawn == true)
         {
             if(currentCountWave <= 10)
             {
@@ -32,7 +36,14 @@ public class SpawnMobController : MonoBehaviour
 
             mob.transform.position = new Vector2(-268.1f, 259.4f);
 
-            currentCountMobsOnWave++;  
+            currentCountMobsOnWave++;
+            canSpawn = false;
         }
+    }
+    
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("ddd");
+        canSpawn = true;
     }
 }
