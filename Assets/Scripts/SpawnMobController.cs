b8ce6e0d171cb5a0fff63e185414681b3f6bcf21
unknown <stas.nikolaev.2019@gmail.com>
2023-02-10 20:00:02 +0300

    changed code v12

--- Assets/Scripts/SpawnMobController.cs
+++ Assets/Scripts/SpawnMobController.cs
@@ -2,55 +2,5 @@ using UnityEngine;
 
 public class SpawnMobController : MonoBehaviour
 {
-    [HideInInspector]
-    private int currentCountWave;
-    [SerializeField]
-    private Vector2 positionSpawn;
-    [SerializeField]
-    private GameObject[] mobs;
-    [SerializeField]
-    private bool canSpawn;
-    [SerializeField]
-    private int maxCountMobs;
-    private GameObject mob;
-    [HideInInspector]
-    public int currentCountMobsOnWave;
-    public Transform[] wayPoints;
-
-    void Start()
-    { 
-        positionSpawn = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
-        canSpawn = true;
-    }
-
-    void Update() => Spawn();
-
-    private void Spawn()
-    {
-        if(currentCountMobsOnWave != maxCountMobs && canSpawn == true)
-        {
-            if(currentCountWave <= 10)
-            {
-                mob = Instantiate(mobs[0]);
-            }
-            else
-            {
-                mob = Instantiate(mobs[Random.Range(0, 3)]);
-            }
-
-            mob.GetComponent<EnemyController>().wayPoints = wayPoints;
-
-            mob.transform.position = positionSpawn;
-
-            currentCountMobsOnWave++;
-            canSpawn = false;
-        }
-        canSpawn = CanSpawnMethod();
-    }
-
-    private bool CanSpawnMethod()
-    {
-        bool f = false;
-        return f = GameObject.FindGameObjectWithTag("CanSpawnBlock");
-    }
+    
 }
