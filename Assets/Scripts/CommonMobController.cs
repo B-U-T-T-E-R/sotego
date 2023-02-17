20c0e9f95828512a3372edb4e7d87c255748e69c
unknown <stas.nikolaev.2019@gmail.com>
2023-02-17 20:09:35 +0300

    changed code v14

--- Assets/Scripts/CommonMobController.cs
+++ Assets/Scripts/CommonMobController.cs
@@ -2,12 +2,5 @@ using UnityEngine;
 
 public class CommonMobController : EnemyController
 {
-    [SerializeField]
-    private Transform mob;
-
-    private void Start()
-    {
-        mob = GetComponent<Transform>();
-    }
-    void Update() => Moving(50f, mob);
+    void Update() => Moving(50f);
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,32 +1,29 @@
 using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
-    private int currentwayPoints = 0;
-    private float lastWaypointSwitchTime;
+    int nextwayPoints = 0;
     public Transform[] wayPoints;
+    private Rigidbody2D rb;
 
-    public void Moving(float mspeed, Transform h)
+    private void Start()
     {
-        Vector3 startPosition = wayPoints[currentwayPoints].transform.position;
-        Vector3 endPosition = wayPoints[currentwayPoints + 1].transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-        float pathLength = Vector3.Distance(startPosition, endPosition);
-        float totalTimeForPath = pathLength / mspeed;
-        float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
-        h.position = Vector2.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+    public void Moving(float mspeed)
+    {
 
-        if (h.position.Equals(endPosition))
-        {
-            if (currentwayPoints < wayPoints.Length - 2)
-            {
-                currentwayPoints++;
-                lastWaypointSwitchTime = Time.time;
-            }
-            else
-            {
-                Die();
-            }
-        }
+        if (this.gameObject.transform.position == wayPoints[nextwayPoints].position)
+            nextwayPoints++;
+
+        if (this.gameObject.transform.position.y > wayPoints[nextwayPoints].position.y)
+            rb.AddForce(new Vector2(0, -mspeed));
+        else
+            rb.AddForce(new Vector2(0, mspeed));
+        if (this.gameObject.transform.position.x > wayPoints[nextwayPoints].position.x)
+            rb.AddForce(new Vector2(-mspeed, 0));
+        else
+            rb.AddForce(new Vector2(mspeed, 0));
     }
 
     void Die()
